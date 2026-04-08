e19cec02b745af2e5d27bb3c035abe50d01b4f4c
SzymonPietraszek <SzymonPietraszek1997@gmail.com>
2026-04-08 09:51:46 +0200

    Add api router

--- app/main.py
+++ app/main.py
@@ -1,15 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from app.routers import hello, dynamo
 from mangum import Mangum
 
 app = FastAPI()
-app.include_router(hello.router)
-app.include_router(dynamo.router)
+api_router = APIRouter(prefix="/api")
+api_router.include_router(hello.router)
+api_router.include_router(dynamo.router)
 
 
-@app.get("/")
+@api_router.get("/")
 async def root():
     return {"message": "Hello World from Root!"}
 
 
+app.include_router(api_router)
 handler = Mangum(app)
